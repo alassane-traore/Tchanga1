911868f41eb4abf44355f50d4a38b288323be990
alassane-traore <alassanet076@gmail.com>
2024-01-10 11:34:30 +0100

    debug true second

--- Tchanga/settings.py
+++ Tchanga/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['tchanga12x.onrender.com']
 
