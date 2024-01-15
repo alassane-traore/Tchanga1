74f668fd118cb99a06dc39409ea610f3b55c4eb6
alassane-traore <alassanet076@gmail.com>
2024-01-15 11:11:53 +0100

    DEBUG defitively False

--- Tchanga/settings.py
+++ Tchanga/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =os.environ.get('SECRET_KEY'), parse_qs(os.environ.get('SECRET_KEY'))#'django-insecure-ph+676$%oyyv73(vr#!q-s@t_p6rhow8$6$rg_&*7@09+_%jy1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =os.environ.get("DEBUG",False)
+DEBUG =False #os.environ.get("DEBUG",False)
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS","127.0.0.1 ").split(" ")
 
