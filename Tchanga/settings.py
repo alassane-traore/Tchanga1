7f6394af6b1b569f2f6c4853d3fbb089ce3fed4b
alassane-traore <alassanet076@gmail.com>
2024-01-15 12:50:29 +0100

    staticdirs updated

--- Tchanga/settings.py
+++ Tchanga/settings.py
@@ -140,9 +140,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'plan/static'),
-    os.path.join(BASE_DIR,'kasse/static'),
-    os.path.join(BASE_DIR,'users/static')
+    os.path.join(BASE_DIR, 'plan', 'static'),
+    os.path.join(BASE_DIR, 'users', 'static'),
+    os.path.join(BASE_DIR, 'kasse', 'static'),
                   ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
