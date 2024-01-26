6a34ec0c233de193b3f60f6703447ea12a5e4c69
alassane-traore <alassanet076@gmail.com>
2024-01-26 07:55:57 +0100

    sessions config

--- Tchanga/settings.py
+++ Tchanga/settings.py
@@ -153,3 +153,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+SESSION_COOKIE_NAME = 'Tchanga_session'
