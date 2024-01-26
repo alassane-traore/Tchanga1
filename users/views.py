8930d978d287d2810df6a0ef48c869e9eef02e4c
alassane-traore <alassanet076@gmail.com>
2024-01-26 22:30:37 +0100

    print  get session

--- users/views.py
+++ users/views.py
@@ -68,7 +68,7 @@ def home(req):
     #return render(req, "users/home.html")
 
 def signup(request):
-    print("session:",request.session)
+    print("session:",request.session.get())
      
     if request.method == "POST":
         post1=request.POST
