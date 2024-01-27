f49fecaca1aebbb6d5f0fa6c9609f5cfa5d387de
alassane-traore <alassanet076@gmail.com>
2024-01-27 01:00:13 +0100

    debug set to true

--- Tchanga/settings.py
+++ Tchanga/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY =os.environ.get('SECRET_KEY'), parse_qs(os.environ.get('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =False #os.environ.get("DEBUG",False)
+DEBUG =True #os.environ.get("DEBUG",False)
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS","127.0.0.1 ").split(" ")
 
