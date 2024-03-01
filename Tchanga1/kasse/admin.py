1c0dbd0eb18e15dd57eab65bb6ed72dace35fefd
alassane-traore <alassanet076@gmail.com>
2024-03-01 23:46:00 +0100

    Delete Tchanga1/kasse directory

--- Tchanga1/kasse/admin.py
+++
@@ -1,7 +0,0 @@
-"""from django.contrib import admin
-from .models import Basket,Sector,Goods
-# Register your models here.
-admin.site.register(Basket)
-admin.site.register(Sector)
-admin.site.register(Goods)
-"""
